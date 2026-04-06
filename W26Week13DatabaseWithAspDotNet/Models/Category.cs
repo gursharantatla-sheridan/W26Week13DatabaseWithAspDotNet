9b9d3d750b63f0209592426e5a0b9a020dabbdc8
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-04-06 13:21:13 -0400

    models

---
+++ W26Week13DatabaseWithAspDotNet/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace W26Week13DatabaseWithAspDotNet.Models
+{
+    public class Category
+    {
+        // scalar properties
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        // navigation property
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ W26Week13DatabaseWithAspDotNet/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace W26Week13DatabaseWithAspDotNet.Models
+{
+    public class Product
+    {
+        // scalar properties
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public double? Price { get; set; }
+        public int? CategoryId { get; set; }
+
+        // navigation property
+        public Category? Category { get; set; }
+    }
+}
